699484005e5d80126e90ae6486f593435c1e869a
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-21 23:40:08 -0400

    Added global variables

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// ------------------------ GLOBAL VARIABLES ------------------------
+  // list for guessed letters:
+const guessedLetters = document.querySelector(".guessed-letters");
+  //  Guess button:
+const guessButton = document.querySelector(".guess");
+  // guess letter text input:
+const letter = document.querySelector("#letter");
+  // word progress paragraph:
+const wordProgress = document.querySelector(".word-in-progress");
+  // remaining guesses counter paragraph:
+const remainingGuesses = document.querySelector(".remaining");
+  // remaining guesses counter span:
+const guessCounter = document.querySelector(".remaining span");
+  // guess message paragraph:
+const message = document.querySelector(".message");
+  // play again button:
+const playAgain = document.querySelector(".play-again");
+  // starting word:
+const word = "magnolia";
+
