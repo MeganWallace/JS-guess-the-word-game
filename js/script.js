8c013e03754b929006a29ea2998023e17daa3b7f
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-22 15:46:47 -0400

    Added function to capture guess input and add guess to guessed letters array (or return try again message)

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const word = "magnolia";
 // empty array to hold guessed letters
 const guessedLetters = [];
 
-// ===================== Placeholder Function for Guess Word Letters =====================
+// ===================== Placeholder Function for Word Letters =====================
 const placeholder = function (word) { //function to display placeholder symbols instead of letters for guess word
   const letters = []; //empty array to hold individual letters of word
   for (let letter of word) { //loops through each letter of word
@@ -37,12 +37,15 @@ placeholder(word); //calls placeholder function to display placeholder symbols i
 guessButton.addEventListener("click", function (e) {
   e.preventDefault(); //prevents the default form behavior of clicking a button, the form submitting, and then reloading the page...this allows the previously guessed letters to be kept
   const guess = letterInput.value; //captures the letter input box value
-      // DON'T NEED ANYMORE --> console.log(guess); //logs the letter input value (placeholder)
   letterInput.value = ""; //clears the letter input box
 
   message.innerText = ""; //clears the message paragraph
   const validationResult = validateInput(guess); //validates letter input from player and assigns variable to result
-  console.log(validationResult); //logs validation result (placeholder)
+  // console.log(validationResult); //logs validation result (placeholder)
+
+  if (validationResult){ //if guess is valid (ie. validation results in a letter, not undefined)
+    makeGuess(guess); //run makeGuess function (ie. add to guessedLetters array or show try again message)
+  }
 });
 
 
@@ -52,13 +55,26 @@ const validateInput = function (input) {
 
   if (input === "") { //checks if input is empty
         // alt. solution code --> if (input.length === 0)
-    message.innerText = "Please enter a guess.";
+    message.innerText = "Please enter a guess."; //returns error message if true
   } else if (input.length > 1) { //checks if input has more than one character
-    message.innerText = "Please only guess one letter at a time.";
+    message.innerText = "Please only guess one letter at a time."; //returns error message if true
   } else if (input.match(acceptedLetter) === null) {  //checks if input doesn't match regular expression
         // alt. solution code --> else if (!input.match(acceptedLetter))
-    message.innerText = "Please guess a letter from A to Z.";
+    message.innerText = "Please guess a letter from A to Z."; //returns error message if true
   } else {
-    return input; //returns input if good
+    return input; //otherwise returns input
+  }
+};
+
+// ===================== Function to Capture Guess Input =====================
+
+const makeGuess = function(guess){ //reuse "guess" variable from click event function
+  guess = guess.toUpperCase(); //convert guess to uppercase
+
+  if (guessedLetters.includes(guess)){ //if guessedLetters array includes the guess
+    message.innerText = "You already guessed that letter! Try again."; //returns try again message
+  } else {
+    guessedLetters.push(guess); //otherwise guess is added to guessedLetters array
+    console.log(guessedLetters); //log guessedLetters array
   }
 };
